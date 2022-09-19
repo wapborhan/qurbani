5fda0b9ae3d264c124036366002c2a96cf61a4cb
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-09-19 12:54:32 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,61 @@
+import React, { useState } from "react";
+import MainComponents from "./components/MainComponents";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
+import { FaBars, FaUserCircle, FaAlignLeft } from "react-icons/fa";
+
+import { BrowserRouter } from "react-router-dom";
+
 function App() {
-  return <div className="App">টেস্ট</div>;
+  const [isActive, setActive] = useState("false");
+  const handleToggle = () => {
+    setActive(!isActive);
+  };
+  return (
+    <BrowserRouter>
+      <div id="wrapper">
+        <div
+          className={
+            isActive
+              ? "sidebars bg-dark text-light  "
+              : "sidebars bg-dark text-light active"
+          }
+        >
+          <Sidebar />
+        </div>
+        <div id="content-wrapper" className="d-flex flex-column">
+          <div id="content">
+            <nav
+              id="topbar"
+              className="topbar navbar navbar-expand navbar-light bg-white  mb-4 static-top shadow d-flex justify-content-between px-4"
+            >
+              <div
+                className="sidebars-button text-dark mr-auto"
+                onClick={handleToggle}
+              >
+                {isActive ? <FaBars /> : <FaAlignLeft />}
+                {/* <FaBars /> */}
+                <div className={isActive ? "dashboard" : "dashboard-show"}>
+                  SR BUDGET
+                </div>
+              </div>
+
+              <h2 id="nameTitle" className="text-center text-dark">
+                আয় ব্যয় হিসাব
+              </h2>
+              <div className="account ">
+                <a href="#">
+                  <FaUserCircle />
+                </a>
+              </div>
+            </nav>
+            <MainComponents />
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
