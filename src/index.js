d6013a9dd1f9eb78c71f89a40ecc7e58371a2b9a
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-09-19 12:51:02 +0600

    file create

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+//
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/stylesheet/styles.css";
+import "./assets/stylesheet/sidebar.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
